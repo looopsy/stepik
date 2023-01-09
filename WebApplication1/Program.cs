a26c1188244a664f9b2876c72ca2d6451f711350
looopsy <92292015+looopsy@users.noreply.github.com>
2023-01-09 12:23:12 +0300

    Add files via upload

---
+++ WebApplication1/Program.cs
@@ -0,0 +1,40 @@
+var builder = WebApplication.CreateBuilder(args);
+var app = builder.Build();
+
+app.MapGet("/", async (context) =>
+{
+    context.Response.ContentType = "text/html; charset=utf-8";
+    await context.Response.SendFileAsync("html/index.html");
+});
+
+app.MapGet("/diameter/{dm}/{dl}", diamRequest);
+app.MapGet("/know/{a}/{b}/{c}", perimeterRequest);
+app.Run();
+
+
+string diamRequest(int dm, int dl)
+{
+    double a = Math.Sqrt(Math.Pow(dl, 2) * 2);
+    StreamWriter sw = new StreamWriter("./log.txt", true);
+    if (a <= dm)
+    {
+        sw.WriteLine("Диаметр - " + dm + ", Длинна - " + dl + " Выпилить брусок возможно");
+        sw.Close();
+        return $"Выпилить брусок возможно.";
+
+    }
+    sw.WriteLine("Диаметр - " + dm + ", Длинна - " + dl + " Выпилить брусок невозможно");
+    sw.Close();
+    return $"Выпилить брусок невозможно.";
+}
+
+string perimeterRequest(int a, int b, int c)
+{
+    int p = a+b+c;
+    StreamWriter sw = new StreamWriter("./log.txt", true);
+    double s = Math.Sqrt(p * (p - a) * (p - b) * (p - c));
+    sw.WriteLine("A -" + a + ", B - " + b + ", C - " + c + $" Периметр: {p}, Площадь: {s}");
+    sw.Close();
+    return $"Периметр: {p}, Площадь: {s}";
+
+}
